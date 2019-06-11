931f03bc36e2a6843dd366950d3614ab630a964a
Daniel Krusch <daniel.krusch@gmail.com>
2019-06-11 14:15:31 -0500

    Added some comments to describe what we are doing

---
+++ src/scripts/component.js
@@ -0,0 +1,1 @@
+// Query selectors for all elements and create string literals
---
+++ src/scripts/key-template.js
@@ -0,0 +1,25 @@
+// Fill this with a personal key object
+const zomatoKeys = 
+{
+    appId: `YOUR ID`,
+    appKey: `YOUR KEY`
+}
+
+const eventKeys =
+{
+    appId: `YOUR ID`,
+    appKey: `YOUR KEY`
+}
+
+const ticketKeys =
+{
+    appId: `YOUR ID`,
+    appKey: `YOUR KEY`
+}
+
+const metroKeys =
+{
+    appId: `YOUR ID`,
+    appKey: `YOUR KEY`
+}
+
