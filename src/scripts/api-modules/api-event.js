125fb9e7580f942b300896e01172209f1c3f9e33
joekennerly <joseph.kennerly@gmail.com>
2019-06-12 15:55:30 -0500

    changes

--- src/scripts/api-modules/api-event.js
+++ src/scripts/api-modules/api-event.js
@@ -7,15 +7,17 @@ function getBriteData (searchTerm) {
     }
   })
   .then(jsonData => jsonData.json()).then(convertedData => {
-    
     convertedData.events.forEach(briteMeetup => {
       let briteObject = {}
       briteObject.name = briteMeetup.name.text
       briteSearchResults.push(briteObject)
-
     })
   })
 }
+
+document.querySelector('.meetups').addEventListener('click', ()=> {
+  for (let i = 0; i < briteSearchResults.length; i++)
+})
 console.log(briteSearchResults);
 console.log(inputs);
 getBriteData("categories")
