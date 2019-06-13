4ab97b67fa003a2933278fa7fae051336103ab39
Daniel Krusch <daniel.krusch@gmail.com>
2019-06-13 15:35:01 -0500

    Made put work and have it display on load

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,21 @@
 // Runs function to fetch api with the value of input
 // Then adds it to dom
+const jsonItinerary = {
+    getItinerary: function() {
+      return fetch("http://localhost:3000/itinerary").then(response =>
+        response.json()
+      )
+      .then(data => {
+        console.log(data)
+        parksContainer.textContent = data[0].park
+        foodsContainer.textContent = data[0].food
+        meetupsContainer.textContent = data[0].meetup
+        concertsContainer.textContent = data[0].concert
+      });
+    }
+  };
+
+
+let thing = jsonItinerary.getItinerary()
+
+thing
